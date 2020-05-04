ca7578c0a9c0800cbc81aa52e143db05f187bcdf
José Novais <jlnovais@gmail.com>
2020-05-04 16:46:06 +0100

    teste

--- WSTeste/WSTeste/Program.cs
+++ WSTeste/WSTeste/Program.cs
@@ -9,7 +9,7 @@ namespace WSTeste
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
+        [STAThread] 
         static void Main()
         {
             Application.EnableVisualStyles();
